30b72a7ec17d86c9d33cc55c3b45ca36a043ca34
wooseok <diddmlvkf@gmail.com>
2021-11-25 22:36:02 +0900

    feat(Application) : 임의의 수를 가진 Balls 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Application {
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        int end = 1;
+        Balls computer = createRandomBalls(3);
+
+        while (end == 1) {
+            View.inputUI();
+
+        }
+    }
+
+    public static Balls createRandomBalls(int size) {
+        Balls balls = new Balls(3);
+        Set<Ball> ballSet = new LinkedHashSet<>();
+        while (ballSet.size() < size) {
+            ballSet.add(new Ball(Randoms.pickNumberInRange(1, 9)));
+        }
+        Iterator<Ball> ballIterator = ballSet.iterator();
+        while (ballIterator.hasNext()) {
+            balls.add(ballIterator.next());
+        }
+        return balls;
     }
 
     public static Balls convertBalls(String input) {
         validateInput(input);
         Balls balls = new Balls(3);
         for (int i = 0; i < 3; i++) {
-            balls.add(convertBall(
-                input.charAt(i)
-            ));
+            balls.add(convertBall(input.charAt(i)));
         }
         return balls;
     }
