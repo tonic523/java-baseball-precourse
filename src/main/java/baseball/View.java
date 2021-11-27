3a88d9497d97b144a94ab2716f6c45cbd5b89848
wooseok <diddmlvkf@gmail.com>
2021-11-27 22:23:44 +0900

    refactor(View) : 사용하지 않는 endUI 제거

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,6 +1,7 @@
 package baseball;
 
 public class View {
+
     public static void inputUI() {
         System.out.print("숫자를 입력해주세요 : ");
     }
@@ -26,9 +27,5 @@ public class View {
     public static void correctUI() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
             + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-
-    public static void endUI() {
-        System.out.println("게임 종료");
     }
 }
